aecf400e6e53835c7a9c1ee72ae02fb4d94ef4f7
joshuaholloway <jhollow6@asu.edu>
2019-01-19 17:00:58 -0800

    Fixed with appropriate boundary condition

--- efficient_blur/src/main.cpp
+++ efficient_blur/src/main.cpp
@@ -3,35 +3,56 @@ using std::cout;
 
 // Run matlab script: matlab/blox_blur.m
 
+constexpr size_t N = 4;
+constexpr size_t K = 3;
+
 int main()
 {
-	int x[6 * 6] = {
-		 0, 0, 0, 0, 0, 0,
-		 0, 1, 2, 3, 4, 0,
-		 0, 5, 6, 7, 8, 0,
-		 0, 9,10,11,12, 0,
-		 0, 13,14,15,16,0,
-		 0, 0, 0, 0, 0, 0};
-	int y[6 * 6] = { 0 };
+	//int x[6 * 6] = {
+	//	 0, 0, 0, 0, 0, 0,
+	//	 0, 1, 2, 3, 4, 0,
+	//	 0, 5, 6, 7, 8, 0,
+	//	 0, 9,10,11,12, 0,
+	//	 0, 13,14,15,16,0,
+	//	 0, 0, 0, 0, 0, 0};
+	int x[N * N] = {
+		 1, 2, 3, 4, 
+		 5, 6, 7, 8, 
+		 9,10,11,12, 
+		 13,14,15,16,};
+	int y[N * N] = { 0 };
 
 	auto lin = [](int i, int j, int N) -> int { return i * N + j; };
 
 	int count = 0;
-	for (int i = 1; i < 5; i++)
+	for (int i = 0; i < N; ++i)
 	{
-		for (int j = 0; j < 4; j++)
+		for (int j = 0; j < N; ++j)
 		{
 			int sum = 0;
-			for (int k = 0; k < 3; k++)
-				sum += x[lin(i, j + k, 6)];
-
-			y[count] = sum;
-			cout << y[count] << "\t";
-			count++;
+			for (int k = 0; k < K; ++k)
+			{
+				int kdx = j + k;
+				if (kdx >= 0 && kdx < N)
+					sum += x[lin(i, kdx, N)];
+			}
+			y[lin(i,j,N)] = sum;
+			//cout << y[count] << "\t";
+			//count++;
 		}
 		cout << "\n";
 	}
-	getchar();
+	cout << "\n";
+	
+	// Look at entire intermediate matrix
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			cout << y[lin(i,j,N)] << "\t";
+		cout << "\n";
+	}
+	cout << "\n";
 
+	getchar();
 	return 0;
 }
