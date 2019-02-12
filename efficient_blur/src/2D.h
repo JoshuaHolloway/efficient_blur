699e40de9efa711cc9522bcbb7dd105464a4a490
joshuaholloway <jhollow6@asu.edu>
2019-02-12 03:01:58 -0800

    fused started

--- efficient_blur/src/2D.h
+++ efficient_blur/src/2D.h
@@ -47,6 +47,7 @@ namespace two_D
 	template <size_t rows, size_t cols, typename T>
 	ArrStruct conv(T(&x_zp)[rows][cols])
 	{
+		// (Output is non-zero padded)
 		// 1D-conv of (1xK) box-filter with (1xN_) signal
 		// Write output in transposed form
 		// MATLAB: 
@@ -56,7 +57,6 @@ namespace two_D
 		
 		// Do Convolution
 		ArrStruct y = {};
-		int y_temp[4][4] = {};
 
 		// 1D-conv (row-vector)
 		for (size_t n1 = P; n1 < Q-P; n1++)
@@ -75,12 +75,69 @@ namespace two_D
 				}
 				y.arr[n2][n1 - P] = sum;
 				cout << "- - - - - - - - - - - - - \n";
+				getchar();
 			}
 			int debug = 0;
 		}
 
 		return y;
 	}
+	// - - - - - - - - - - - - - - - - 
+	template <size_t rows, size_t cols, typename T>
+	ArrStruct conv_fused_1(T(&x_zp)[rows][cols])
+	{
+		// 2D-conv with maximum locality
+
+		// Do Convolution
+		ArrStruct y = {};
+
+		// TODO: Change to full intermediate buffer
+		int y_temp[K] = {};
+
+		// 1D-conv (row-vector)
+		for (size_t n1 = P; n1 < Q - P; ++n1)
+		{
+			cout << "===========================\n";
+			cout << "n1 = " << n1 << "\n";
+			cout << "===========================\n";
+			for (size_t n2 = 0; n2 < 4; ++n2)
+			{
+				for (size_t k1 = 0; k1 != K; ++k1)
+				{
+					int sum = 0;
+					for (size_t k2 = 0; k2 != K; ++k2)
+					{
+						sum += x_zp[n1 + k1][n2 + k2];
+						cout << "x_zp[n1 + k1][n2 + k2] = x[" << n1 + k1 << "][" << n2 + k2 << "] =" << x_zp[n1 + k1][n2 + k2] << "\n";
+					}
+					cout << "Store buffer\n";;
+					y_temp[k1] = sum;
+				}
+
+				int sum = 0;
+				for (size_t k1 = 0; k1 != K; ++k1)
+				{
+					cout << "Add redundant\n";;
+					sum += y_temp[k1];
+				}
+
+				y.arr[n2][n1 - P] = sum;
+				cout << "- - - - - - - - - - - - - \n";
+				getchar();
+			}
+			int debug = 0;
+		}
+
+		return y;
+	}
+
+	// TODO: Create new conv-stage-1() func where
+	//       the output is written zero padded in cols
+	//
+	// TODO: Create new conv-stage-2() func that takes as input
+	//       the col-zero padded matrix and writes output
+	//       not zero-padded
+
 	// - - - - - - - - - - - - - - - - 
 	void two_D()
 	{
@@ -97,12 +154,18 @@ namespace two_D
 		// Display
 		print("After zero-padding", x_zp.arr);
 
-		// Do conv
-		ArrStruct y = conv(x_zp.arr);
-		print("After conv (stage-1)", y.arr);
-		ArrStruct_zp y_zp = pad(y.arr);
-		ArrStruct z = conv(y_zp.arr);
-		print("After conv (stage-2)", z.arr);
+		//// Do conv - extreme 1
+		//// TODO: Remove the output written as transpose to 
+		//// more easily compare different extremes
+		//ArrStruct y = conv(x_zp.arr);
+		//print("After conv (stage-1)", y.arr);
+		//ArrStruct_zp y_zp = pad(y.arr);
+		//ArrStruct z = conv(y_zp.arr);
+		//print("After conv (stage-2)", z.arr);
+
+		// Do 
+		cout << "Entering fused conv\n";
+		conv_fused_1(x_zp.arr);
 
 		getchar();
 	}
--- efficient_blur/src/main.cpp
+++ efficient_blur/src/main.cpp
@@ -1,8 +1,13 @@
 #include "2D.h"
 #include "1D.h"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
 // - - - - - - - - - - - - - - - - 
 int main()
 {
+	cv::Mat mat(2,2,CV_32FC1);
+	cv::imshow("mat", mat);
+
 	//one_D::one_D();
 	two_D::two_D();
 	return 0;
